4d5c385cf8e9035b5629cb924bbc757d5548fe22
jokerf6 <fhakem75@gmail.com>
2023-02-28 22:14:23 +0200

    navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Home from './components/Home';
+import React from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <>
-      <div className="App">
-        <Home />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/home/navbar.js
+++ src/components/home/navbar.js
@@ -1,24 +1,48 @@
 import React from "react";
-import "../Home.css"
+import "../Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h3 className="logo">Jo<span>bb</span>er</h3>
-            <ul className="nav-link">
-                <div className="non-but">
-                    <li><a href="#home" className="home">Home</a></li>
-                    <li><a href="#about" className="aboutus">About Us</a></li>
-                    <li><a href="#contact" className="contact">Contact Us</a></li>
-                </div>
-                <div className="butt">
-                    <button className="login">login</button>
-                    <button className="signup">sign up</button>
-                </div>
-
-            </ul>
-
-        </nav>
-    )
-}
+  const navigate = useNavigate();
+  function loginClick() {
+    navigate("/login");
+  }
+  function signupClick() {
+    navigate("/signup");
+  }
+  return (
+    <nav className="navbar">
+      <h3 className="logo">
+        Jo<span>bb</span>er
+      </h3>
+      <ul className="nav-link">
+        <div className="non-but">
+          <li>
+            <a href="#home" className="home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="aboutus">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="contact">
+              Contact Us
+            </a>
+          </li>
+        </div>
+        <div className="butt">
+          <button className="login" onClick={loginClick}>
+            login
+          </button>
+          <button className="signup" onClick={signupClick}>
+            sign up
+          </button>
+        </div>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
