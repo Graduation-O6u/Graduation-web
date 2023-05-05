14722dc8c13d859b4db5ae54fbdbaa48ab1b021d
Amanymohamedsayed <amanymohamed20011@gmail.com>
2023-05-05 08:29:48 +0200

    khv

--- src/screens/meeting/components/cutdown.js
+++ src/screens/meeting/components/cutdown.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import styles from "../meeting.module.css";
 function AppointmentCountdown() {
-  const [appointmentTime, setAppointmentTime] = useState("2023-05-06T14:00:00");
+  const [appointmentTime, setAppointmentTime] = useState("2023-05-05T15:00:00");
   const [countdown, setCountdown] = useState("");
-
   useEffect(() => {
     const interval = setInterval(() => {
       const now = moment();
@@ -21,10 +20,14 @@ function AppointmentCountdown() {
         );
       } else if (diff.asHours() <= 48 && diff.asHours() > 0) {
         setCountdown(
-          ` ${remainingHours} : ${remainingMinutes} : ${remainingSeconds} `
+          `<span>${remainingHours < 10 ? "0" : ""}${remainingHours}:${
+            remainingMinutes < 10 ? "0" : ""
+          }${remainingMinutes}:${
+            remainingSeconds < 10 ? "0" : ""
+          }${remainingSeconds}</span><a href="#" onClick={() => window.open("https://meeting-link.com")}>The link to the meeting</a>`
         );
       } else {
-        setCountdown("Appointment time has passed");
+        setCountdown(" time has passed");
         clearInterval(interval);
       }
     }, 1000);
@@ -36,11 +39,10 @@ function AppointmentCountdown() {
     <div className={styles.container}>
       <div
         className={styles.text}
-        style={{ color: countdown.includes(":") ? "red" : "" }}
+        style={{ color: countdown.includes("M") ? "#666" : "red" }}
         dangerouslySetInnerHTML={{ __html: countdown }}
       ></div>
     </div>
   );
 }
-
 export default AppointmentCountdown;
