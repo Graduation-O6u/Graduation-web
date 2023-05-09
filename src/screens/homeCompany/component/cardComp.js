d74bfa1b286bec8fa75a71ecbafe48a2e83cfec2
Amanymohamedsayed <amanymohamed20011@gmail.com>
2023-05-09 12:18:47 +0200

    ds

--- src/screens/homeCompany/component/cardComp.js
+++ src/screens/homeCompany/component/cardComp.js
@@ -5,7 +5,7 @@ import profile from "../../../images/avatar2.png";
 import { Icon } from "@iconify/react";
 const ProfileCard = () => {
   return (
-    <>
+    <div>
       <div className={styles.containerCard}>
         <h1 className={styles.heading}> Great Staff</h1>
         <div className={styles.cardWrapper}>
@@ -41,203 +41,7 @@ const ProfileCard = () => {
             </ul>
             <p className={styles.about}>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
-            </p>
-            <a className={styles.contant} href="# ">
-              View Profile
-            </a>
-          </div>
-          <div className={styles.card}>
-            <img
-              src={background}
-              alt="card background"
-              className={styles.cardImg}
-            />
-            <img
-              src={profile}
-              alt="card profile"
-              className={styles.profileImg}
-            />
-            <h1 className={styles.cardh1}>Amany Mohamed </h1>
-            <p className={styles.jobTitle}>FrontEnd</p>
-            <ul className={styles.social}>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="octicon:mark-github-16" color="black" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="tabler:file-cv" color="#2c6684" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="devicon:behance" />
-                </a>
-              </li>
-            </ul>
-            <p className={styles.about}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
-            </p>
-            <a className={styles.contant} href="# ">
-              View Profile
-            </a>
-          </div>
-          <div className={styles.card}>
-            <img
-              src={background}
-              alt="card background"
-              className={styles.cardImg}
-            />
-            <img
-              src={profile}
-              alt="card profile"
-              className={styles.profileImg}
-            />
-            <h1 className={styles.cardh1}>Amany Mohamed </h1>
-            <p className={styles.jobTitle}>FrontEnd</p>
-            <ul className={styles.social}>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="octicon:mark-github-16" color="black" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="tabler:file-cv" color="#2c6684" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="devicon:behance" />
-                </a>
-              </li>
-            </ul>
-            <p className={styles.about}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
-            </p>
-            <a className={styles.contant} href="# ">
-              View Profile
-            </a>
-          </div>
-          <div className={styles.card}>
-            <img
-              src={background}
-              alt="card background"
-              className={styles.cardImg}
-            />
-            <img
-              src={profile}
-              alt="card profile"
-              className={styles.profileImg}
-            />
-            <h1 className={styles.cardh1}>Amany Mohamed </h1>
-            <p className={styles.jobTitle}>FrontEnd</p>
-            <ul className={styles.social}>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="octicon:mark-github-16" color="black" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="tabler:file-cv" color="#2c6684" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="devicon:behance" />
-                </a>
-              </li>
-            </ul>
-            <p className={styles.about}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
-            </p>
-            <a className={styles.contant} href="# ">
-              View Profile
-            </a>
-          </div>
-          <div className={styles.card}>
-            <img
-              src={background}
-              alt="card background"
-              className={styles.cardImg}
-            />
-            <img
-              src={profile}
-              alt="card profile"
-              className={styles.profileImg}
-            />
-            <h1 className={styles.cardh1}>Amany Mohamed </h1>
-            <p className={styles.jobTitle}>FrontEnd</p>
-            <ul className={styles.social}>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="octicon:mark-github-16" color="black" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="tabler:file-cv" color="#2c6684" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="devicon:behance" />
-                </a>
-              </li>
-            </ul>
-            <p className={styles.about}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
-            </p>
-            <a className={styles.contant} href="# ">
-              View Profile
-            </a>
-          </div>
-          <div className={styles.card}>
-            <img
-              src={background}
-              alt="card background"
-              className={styles.cardImg}
-            />
-            <img
-              src={profile}
-              alt="card profile"
-              className={styles.profileImg}
-            />
-            <h1 className={styles.cardh1}>Amany Mohamed </h1>
-            <p className={styles.jobTitle}>FrontEnd</p>
-            <ul className={styles.social}>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="octicon:mark-github-16" color="black" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="tabler:file-cv" color="#2c6684" />
-                </a>
-              </li>
-              <li>
-                <a href="# " className={styles.iconsocial}>
-                  <Icon icon="devicon:behance" />
-                </a>
-              </li>
-            </ul>
-            <p className={styles.about}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since.
+              industry.
             </p>
             <a className={styles.contant} href="# ">
               View Profile
@@ -245,7 +49,7 @@ const ProfileCard = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default ProfileCard;
--- src/screens/homeCompany/mainCompany.js
+++ src/screens/homeCompany/mainCompany.js
@@ -1,18 +1,18 @@
 import React from "react";
-import NavbarHome from "../Authentication/homePage/components/Navbar-home" ;
-import FindJob from "../Authentication/homePage/components/FindJob/FindJob" ;
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ProfileCard from "./component/cardComp" ;
-import styles from "./company.module.css" ;
+import NavbarHome from "../Authentication/homePage/components/Navbar-home";
+import FindJob from "../Authentication/homePage/components/FindJob/FindJob";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProfileCard from "./component/cardComp";
+import styles from "./company.module.css";
 const HomeCompany = () => {
-    return (
-        <>
-        <div className={ styles.containerhome}>
-            < NavbarHome titleHerf={"/homecompany"} />
-            < FindJob title={"Staff"} />
-            <ProfileCard />
-        </div> 
-        </>
-);
+  return (
+    <div>
+      <div className={styles.containerhome}>
+        <NavbarHome titleHerf={"/homecompany"} />
+        <FindJob title={"Staff"} />
+        <ProfileCard />
+      </div>
+    </div>
+  );
 };
 export default HomeCompany;
